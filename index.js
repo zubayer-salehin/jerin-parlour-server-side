de336f434403dc62ea7cc352a3b8ebe8aed327fe
zubayer-salehin <freelancerzubear@gmail.com>
2022-11-24 18:40:09 +0600

    database problame soleve

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
 })
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bdkak.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 /*    Verify JWT Start    */
